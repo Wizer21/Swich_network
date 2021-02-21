e265e352f5ca789c8c389f4a1943f5491dee863b
Wizer <simon.tessier8@gmail.com>
2021-02-21 13:13:49 +0100

    Flux publications

--- network/models.py
+++ network/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length = 20)
-    code = models.IntegerField()
+    us_name = models.CharField(max_length = 20)
+    us_password = models.CharField(max_length = 20)
 
 class Publication(models.Model):
-    user = models.CharField(max_length = 20)
+    user_id = models.IntegerField()
+    user_name = models.CharField(max_length = 20)
     text = models.CharField(max_length = 250, default = None)
     image = models.ImageField(default = None)
     date = models.DateTimeField()
--- network/views.py
+++ network/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 
-from .models import User
+
+from .models import User, Publication
 
 user_logged = ""
 
 def home(request):
+    User.objects.all().delete()
+    Publication.objects.all().delete()
     return render(request, "network/flux.html", {
-        "user": user_logged
+        "user": user_logged,
+        "publication_list": Publication.objects.all()
     })
 
 def register(request):
     if request.POST.get("user_name"):
-        if User.objects.filter(name = request.POST.get("user_name")).exists():
-            print("User Already exist")
+        if User.objects.filter(us_name = request.POST.get("user_name")).exists():
             return render(request, "network/register.html", {
                 "error": "User Already exist"
             })
         else:
-            new_user = User(name = request.POST.get("user_name"), code = request.POST.get("password"))
+            new_user = User(us_name = request.POST.get("user_name"), us_password = request.POST.get("password"))
             new_user.save()
-            print("User created")
             return HttpResponseRedirect(reverse('home'))
     else:
         return render(request, "network/register.html")
@@ -32,9 +35,9 @@ def login(request):
     entered_username = request.POST.get("user_name")
     if entered_username:
         # Username entered
-        if User.objects.filter(name = entered_username).exists():            
+        if User.objects.filter(us_name = entered_username).exists():            
             # Username exists
-            user = User.objects.get(name = entered_username)
+            user = User.objects.get(us_name = entered_username)
             if request.POST.get("password") == str(user.code):  
                 # Password valid
                 user_logged = entered_username
@@ -47,3 +50,12 @@ def login(request):
         })
     else:
         return render(request, "network/login.html")
+    
+def publication(request):
+    new_text = request.POST.get("publication_text")
+    if new_text != "":
+        current_user = User.objects.get(us_name = user_logged)
+        new_post = Publication(user_id = current_user.id, user_name = current_user.name, text = new_text) 
+        new_post.save()
+    
+    return HttpResponseRedirect(reverse('home'))     
